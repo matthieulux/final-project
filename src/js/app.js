d88413f198a114cf7a0613061a10796964d4929f
Matthieu <lux.matthieu@gmail.com>
2019-01-15 23:34:29 +0000

    update to remove old code

--- src/js/app.js
+++ src/js/app.js
@@ -51,20 +51,6 @@ App = {
   },
 
   initContract: function() {
-    
-    /*
-    $.getJSON('Adoption.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-    
-      // Set the provider for our contract
-      App.contracts.Adoption.setProvider(App.web3Provider);
-    
-      // Use our contract to retrieve and mark the adopted pets
-      return App.markSold();
-    });
-    */
 
     $.getJSON('Delivery.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract
